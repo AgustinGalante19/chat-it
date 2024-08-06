72f84e0ddbde5e4579a2e732acb34e445d79bc49
Agustin Galante <agustin.galante.19@outlook.es>
2024-08-06 12:39:48 -0300

    refactor(backend): separe the object message from te ws.send

--- proyects/chat-it-backend/server.ts
+++ proyects/chat-it-backend/server.ts
@@ -2,11 +2,6 @@ import { type WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8000 });
 
-interface Client {
-  id: string;
-  socket: WebSocket;
-}
-
 type Event = 'message' | 'register';
 
 interface GenericEvent {
@@ -14,34 +9,44 @@ interface GenericEvent {
   id: string;
 }
 
-interface MessageEvent extends GenericEvent {
+interface Message extends GenericEvent {
   toId: string;
   content: string;
+}
+
+interface Client {
+  id: string;
+  socket: WebSocket;
 }
 
 const clients: Client[] = [];
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    const data = JSON.parse(message.toString()) as MessageEvent;
+    const data = JSON.parse(message.toString()) as Message;
     if (data.type === 'register') {
       const client: Client = { id: data.id, socket: ws };
       clients.push(client);
       console.log(`Client ${data.id} connected`);
+      ws.send(
+        JSON.stringify({
+          type: 'register',
+          id: client.id,
+        })
+      );
     } else if (data.type === 'message') {
       const recipient = clients.find((e) => e.id === data.toId);
       if (recipient) {
         const currentDate = new Date();
         const currentTime = currentDate.toLocaleTimeString();
         const time = currentTime.split(':').splice(0, 2).join(':');
-        recipient.socket.send(
-          JSON.stringify({
-            id: data.id,
-            content: data.content,
-            toId: data.toId,
-            time,
-          })
-        );
+        const message = {
+          id: data.id,
+          content: data.content,
+          toId: data.toId,
+          time,
+        };
+        recipient.socket.send(JSON.stringify(message));
       }
     }
   });
